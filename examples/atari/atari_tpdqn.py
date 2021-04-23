3a96b4b8f0f446baa15121bad276939a849bb543
Zhenghai Xue <181220063@smail.nju.edu.cn>
2021-04-23 15:17:13 +0800

    increase the default test num

--- examples/atari/atari_tpdqn.py
+++ examples/atari/atari_tpdqn.py
@@ -36,7 +36,7 @@ def get_args():
     parser.add_argument('--update-per-step', type=float, default=0.1)
     parser.add_argument('--batch-size', type=int, default=32)
     parser.add_argument('--training-num', type=int, default=10)
-    parser.add_argument('--test-num', type=int, default=10)
+    parser.add_argument('--test-num', type=int, default=50)
     parser.add_argument('--logdir', type=str, default='log')
     parser.add_argument('--exp', type=str, default='exp')
     parser.add_argument('--render', type=float, default=0.)
